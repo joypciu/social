d18936a8aa7c990144671b8f51bea06279c9457d
usmanjoy <usmanjoy89@gmail.com>
2023-01-02 23:28:42 +0600

    created endpoints for activity model/entites and also seeded some dummy data

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/{Controller}")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
